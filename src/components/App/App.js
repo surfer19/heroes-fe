c486a4a86bbed7e91a8f9b707195be1e8bb32840
Marian <marianmrva123@gmail.com>
2019-10-02 10:44:02 +0200

    Add routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from '../../redux/configureStore';
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import './App.css';
 
 import GnomeList from "../GnomeList/index";
+import GnomeDetail from "../GnomeDetail/index"
 
 const store = configureStore()
 
 function App() {
   return (
     <Provider store={store}>
-      <GnomeList/>
+      <Router>
+        <Route exact path="/" component={GnomeList} />
+        <Route path="/detail/:id" component={GnomeDetail} />
+      </Router>
     </Provider>
   );
 }
