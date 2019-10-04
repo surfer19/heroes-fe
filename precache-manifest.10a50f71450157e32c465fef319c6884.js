50c891f598b2ee2fa599540414b18248c36f73c3
Marian <marianmrva123@gmail.com>
2019-10-04 11:38:00 +0200

    Updates

---
+++ precache-manifest.10a50f71450157e32c465fef319c6884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9991beb61729c511581190c6da279c9b",
+    "url": "/surfer19/heroes-fe/index.html"
+  },
+  {
+    "revision": "2d9aeae11e4fb1a28d55",
+    "url": "/surfer19/heroes-fe/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "d98d73bde98ea21826d0",
+    "url": "/surfer19/heroes-fe/static/css/main.9ed39a19.chunk.css"
+  },
+  {
+    "revision": "2d9aeae11e4fb1a28d55",
+    "url": "/surfer19/heroes-fe/static/js/2.e3c45895.chunk.js"
+  },
+  {
+    "revision": "d98d73bde98ea21826d0",
+    "url": "/surfer19/heroes-fe/static/js/main.ecbc44c6.chunk.js"
+  },
+  {
+    "revision": "9fc84140d634a617f301",
+    "url": "/surfer19/heroes-fe/static/js/runtime-main.2c673e9f.js"
+  }
+]);
